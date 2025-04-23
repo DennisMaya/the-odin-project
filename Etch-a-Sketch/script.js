4ee1dd722a8702d2c46ecf7711258e6c0d37eb8e
Dennis Maya <MayaDennis101@gmail.com>
2025-04-22 23:51:30 -0700

    16 x 16 grid is initialized on page load. Updating the grid size deletes the existing divs and updates with new grid.

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -13,8 +13,33 @@ function handleClick(){
 }
 
 function updateGridSize(gridSize){
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        row.remove();
+    });
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.remove();
+    });
+    
+    const gridContainer = document.getElementById("gridContainer");
     for (let i = 0; i < gridSize; i++){
-        
+        const row = document.createElement("div");
+        row.className = "row";
+        gridContainer.appendChild(row)
+        for(let j = 0; j < gridSize; j++){
+            const box = document.createElement("div");
+            box.className = "box"
+            row.appendChild(box);
+            console.log("row: " + (i + 1) + " col: " + (j + 1) + ": Div Created");   
+        }
     }
 }
+
+function setup(){
+    var initialGridSize = 16;
+    updateGridSize(initialGridSize);
+}
+
+setup();
 
