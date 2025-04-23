51ed3b3cb9ca13e067c9be74b18b39ffe1dff54a
Dennis Maya <MayaDennis101@gmail.com>
2025-04-23 01:58:12 -0700

    Fixed logic for boxes to change color on hover. Removed borders from the boxes.

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -2,11 +2,6 @@ const updateGridBtn = document.getElementById("updateGridBtn");
 
 updateGridBtn.addEventListener('click', () => handleClick());
 
-function handleHover(elem){
-    console.log("hover");
-    elem.className = "usedBox";
-}
-
 function handleClick(){
     do{
         var gridSize = parseInt(window.prompt("How many boxes do you want per side of this grid.\nEnter a number from 16 to 100:"));
@@ -29,22 +24,19 @@ function updateGridSize(gridSize){
     const gridContainer = document.getElementById("gridContainer");
     for (let i = 0; i < gridSize; i++){
         const row = document.createElement("div");
-        row.className = "row";
-        gridContainer.appendChild(row)
+        row.classList.add("row");
         for(let j = 0; j < gridSize; j++){
             const box = document.createElement("div");
-            box.className = "box";
+            box.classList.add("box");
             // box.id = "row: " + (i + 1) + " col: " + (j + 1);
             row.appendChild(box);
+            box.addEventListener("mouseover", () => {
+                box.style.backgroundColor = "red";
+            })
             // console.log("row: " + (i + 1) + " col: " + (j + 1) + ": Div Created");   
         }
+        gridContainer.appendChild(row)
     }
-    const elements = document.querySelectorAll(".box");
-    // console.log(elements);
-    elements.forEach(elem => {
-        // console.log(elem);
-        elem.addEventListener("mouseenter", handleHover(elem));
-    });
 }
 
 function setup(){
