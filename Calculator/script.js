021e650b8cc6c4e93329787920d297d85e71a005
Dennis Maya <MayaDennis101@gmail.com>
2025-07-22 16:42:33 -0700

    calculator input now displays the selected operators but still needs work

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,11 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const inputBox = document.querySelector(".inputBox");
-    let currentInput = "";
-    document.querySelectorAll(".numBtn").forEach(button => {
-        button.addEventListener("click", function () {
-            currentInput += this.textContent;
-            inputBox.textConent = currentInput;
-        });
+const numButtons = document.querySelectorAll('.numBtn');
+const clearBtn = document.getElementById('clearBtn');
+const inputBox = document.querySelector('.inputBox');
+const operatorBtns = document.querySelectorAll('.operator');
+var input = '';
+
+numButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        input += this.textContent;
+        handleClick(input);
     });
 });
-    
+
+clearBtn.addEventListener('click', function(){
+    input = '';
+    handleClick(input);
+});
+
+operatorBtns.forEach(operator => {
+    operator.addEventListener('click', function(){
+        if(operator != clearBtn){
+            input += this.textContent;
+            console.log(input);
+            handleClick(input);
+        }
+    });
+});
+
+function handleClick(input){
+    inputBox.textContent = input;
+}
+
