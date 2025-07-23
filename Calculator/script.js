d29c9fbe998cafd66474195abef70e533401c73f
Dennis Maya <MayaDennis101@gmail.com>
2025-07-22 18:04:34 -0700

    Updated html and code to include onclick handling. Updated class names in html and css.

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,32 +1,50 @@
-const numButtons = document.querySelectorAll('.numBtn');
-const clearBtn = document.getElementById('clearBtn');
-const inputBox = document.querySelector('.inputBox');
-const operatorBtns = document.querySelectorAll('.operator');
-var input = '';
+let display = document.getElementById("display");
 
-numButtons.forEach(button => {
-    button.addEventListener('click', function(){
-        input += this.textContent;
-        handleClick(input);
-    });
-});
-
-clearBtn.addEventListener('click', function(){
-    input = '';
-    handleClick(input);
-});
-
-operatorBtns.forEach(operator => {
-    operator.addEventListener('click', function(){
-        if(operator != clearBtn){
-            input += this.textContent;
-            console.log(input);
-            handleClick(input);
-        }
-    });
-});
-
-function handleClick(input){
-    inputBox.textContent = input;
+function appendValue(value){
+    display.value += value;
 }
 
+function clearDisplay(){
+    display.value = ""
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function operate(a,b,operator){
+    result = null;
+    switch(operator) {
+        case '+':
+            result = add(a,b);
+            break;
+        case '-':
+            result = subtract(a,b);
+            break;
+        case '*':
+            result = multiply(a,b);
+            break;
+        case '/':
+            if(b!==0){
+                result = divide(a,b);
+            }
+            break;
+    }
+    return result;
+}
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a- b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
